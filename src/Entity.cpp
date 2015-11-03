10acedbc5314235b09888aaa5b37d63308b48957
MatthewClark <mclar053@gold.ac.uk>
2015-11-03 13:21:47 +0000

    Added Entity cpp files

---
+++ src/Entity.cpp
@@ -0,0 +1,9 @@
+//
+//  Entity.cpp
+//  UltraCrateBox
+//
+//  Created by Matthew Clark on 03/11/2015.
+//
+//
+
+#include "Entity.h"
---
+++ src/Entity.h
@@ -0,0 +1,14 @@
+//
+//  Entity.h
+//  UltraCrateBox
+//
+//  Created by Matthew Clark on 03/11/2015.
+//
+//
+
+#ifndef __UltraCrateBox__Entity__
+#define __UltraCrateBox__Entity__
+
+#include <stdio.h>
+
+#endif /* defined(__UltraCrateBox__Entity__) */
